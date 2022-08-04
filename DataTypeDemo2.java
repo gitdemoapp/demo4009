47edac1268e84a901d0f70068a401c8bbdf74955
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-08-04 09:45:30 +0530

    Data type example

---
+++ DataTypeDemo2.java
@@ -0,0 +1,69 @@
+/*
+     Language Fundamentals:
+	 ---------------------
+
+	 1. identifier
+	 2. resreved words
+
+
+	 3. data types
+
+	         A. primitve 
+			 
+			      |-Numeric DT
+				          |-integral    (101 , 200000, 937647654, 208024)
+
+						       a)  byte  ---  1 Byte   =8  bits  ==>-128   to 127
+							   b)  short ---  2 Byte   =16  bits ===>-32768  32767
+							   c)  int   ---  4 Byte   =32  bits  ==>-2147483648   to  2147483647
+							   d)  long  ---  8 Byte   =64   bits  ==>-9223372036854775808   to 9223372036854775807
+						  |-floating point   (101.20 , 2348.09....)
+
+						      a) float   --   4 Byte  ---1.234567     f|F   10.0f
+							  b) double  --   8 Byte  ---1.23427435745665 |  10.0
+
+				  |- Non  numeric DT:
+				          |-boolean   (true)
+						  |-char   
+
+				
+
+			 B. Non primitive
+
+
+
+
+	   
+boolean:  true  | false
+  true--1
+  false--0
+
+  size:  NA
+  range: NA  (true , false)
+
+char :-
+
+size: 2 Byte 
+
+range:  0 to 65535
+
+
+
+
+
+
+*/
+
+class DataTypeDemo2
+{
+	public static void main(String[] args){
+
+	     
+
+	      System.out.println(true+true);
+
+
+		
+	
+	}
+}
