fdd9d05ba7ea549e97ee36fdddd0b1f337e9c9ef
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-08-06 10:24:31 +0530

    Literals

---
+++ LiteralsDemo2.java
@@ -0,0 +1,117 @@
+/*
+    
+	integral:
+	===========
+	byte
+	short
+	int
+	long
+
+	floating point literals:
+	--------------------
+
+	float  10.0f
+
+	double  10.0
+
+	1.  double   d=10.0;
+
+	    float   a=10.0f;
+
+		-----------------------
+        decimal form
+
+		010.0--->10.0
+
+		010--->8
+
+		0x10--->
+
+		0x10.0--->error
+
+		0b10.0--->error
+
+         <---------valid
+		float   a=  10;
+
+		double   a=  10;
+
+          <----------- invalid
+           int   a=  10.0;
+
+char literals:
+-----------------
+
+       char  a='a';
+*/
+
+class LiteralsDemo2 
+{
+	public static void main(String[] args) 
+	{
+	      // double  d=10;
+
+	     // double  d=10.0;
+
+		 // float  d= 10;
+
+		 // float  d= 10.0F;
+
+		 // double d=  0786;
+
+		  //double d=  0786.0;
+
+		 //  float  a=10.0f;
+
+		  //  double d=  0xface;
+
+		  // double d= 0x786.0;
+
+		  //  double d= 0x786abc;
+
+		//char  section1=  '1';  //single character within single quotes.
+
+		 // char   x=   "12";
+
+		 //0 to 65535  | 65   | 97
+
+		 //  char  ch=97;
+
+
+		//char  ch='\u00ab';
+		
+
+		//boolean literals:
+/*
+
+		boolean  b=0;
+
+		boolean  b=1;
+
+		boolean  b=10;
+
+		boolean  b=False;
+
+		boolean  b=false;
+
+		boolean  b=true;
+
+		 String literals:
+		 ------------------
+
+		String  s1= "kjfhkdhfkdj";
+
+		String  name="Raja  singh";
+
+		String  fatherName="fraja";
+
+		String address="kanpur";
+
+		String address="kanpur nagar";
+
+   */
+          int  rollNumber=104638746;
+
+		  System.out.println(rollNumber);
+	}
+}
