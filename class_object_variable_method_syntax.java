0f8cbe3a3d110bb29a21611f5e080cd7d45c3274
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-08-13 10:03:34 +0530

    class class_object_variable_method_syntax

---
+++ class_object_variable_method_syntax.java
@@ -0,0 +1,106 @@
+/*
+    Synatx for class:
+	---------------------
+
+
+	class  ClassName{
+	
+	 //properties/variable/state /data 
+
+	 //functionalities/methods/function/member function/behaviour
+	
+	}
+
+	eg:
+
+	class  Student{
+	
+	
+	}
+
+	class  Doctor{
+	
+	
+	}
+
+	class Patient{
+	
+	
+	}
+
+	============================================
+
+    Syntax for properties/variable:
+
+	datatype   variableName=value;
+
+	datatype   variablename;
+
+
+	================================================
+
+   Syntax for methods:
+   -------------------------
+
+   returnType   methodName(Parameters){
+   
+       //business logic
+   
+   }
+
+eg:
+   void   login(){
+   
+   
+   } 
+
+
+   void  register(){
+   
+   
+   }
+
+   void  addition(){
+   
+   
+   }
+
+   void sub(){
+   
+   
+   }
+
+   void  div(){
+   
+   
+   }
+
+
+==========================================================
+
+Syntax for object:
+---------------------
+
+ClassName   variable=new ClassName();
+
+Student   s1=new Student();
+
+
+Doctor   doctor1=new Doctor();
+
+
+Patient  p1=new Patient();
+
+
+Student   s2=new Student();
+
+
+Doctor   doctor2=new Doctor();
+
+
+Patient  p2=new Patient();
+
+
+
+
+*/
