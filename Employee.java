811ce5f4383539fadd5ffde3843c1cf630faf975
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-08-18 09:45:40 +0530

    main method

---
+++ Employee.java
@@ -0,0 +1,29 @@
+class Employee
+{
+	
+	 public  static  void main(String     []a){
+
+	  
+	  System.out.println("This is a valid main method  ==>String[]");
+
+		
+	}
+
+}
+
+/*
+1.  in the place of public static we can take  static public  
+
+2.  args==>
+
+3.  []==>...
+
+4. strictfp synchronized final 
+
+5.  
+
+6. 
+ 
+
+*/
+
