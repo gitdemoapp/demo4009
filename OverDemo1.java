a67c3f8b93026c4712b7493893f1277e7c68090f
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-09-05 10:23:20 +0530

    overriding and oveloading

---
+++ OverDemo1.java
@@ -0,0 +1,34 @@
+
+/*
+
+   Overriding:
+   -----------
+
+   Why?
+
+Case-1:
+-------
+
+*/
+
+class Parent
+{
+	 void  marry(){
+	    System.out.println("we should marry according to parent choice");
+	 }
+}
+
+class Child1 extends Parent
+{
+	
+}
+
+
+
+class OverDemo1 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
---
+++ OverDemo2.java
@@ -0,0 +1,15 @@
+class OverDemo2 
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb1=new StringBuffer("java");
+		StringBuffer sb2=new StringBuffer("java");
+
+		System.out.println(sb1.equals(sb2));
+
+		String  s1=new String("java");
+		String  s2=new String("java");
+
+		System.out.println(s1.equals(s2));
+	}
+}
